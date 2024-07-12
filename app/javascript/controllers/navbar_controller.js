d15ba69b34178269637fc2248507e5e6b8b07e58
Miriam Ciurea <miriamciurea777@icloud.com>
2024-07-12 19:09:45 +0300

    responsive

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["navbar", "icon", "details"]
+
+  connect() {
+    console.log("Controller connected");
+    this.initialOffsetTop = this.navbarTarget.offsetTop;
+    this.scrolled = false;
+
+    console.log(this.initialOffsetTop);
+    console.log(this.iconTarget);
+    console.log(this.detailsTarget);
+
+    // Initial check
+    this.updateNavbarVisibility();
+
+    // Update visibility on resize
+    window.addEventListener('resize', this.updateNavbarVisibility.bind(this));
+
+    // Scroll event listener
+    window.addEventListener('scroll', this.scroll.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener('scroll', this.scroll.bind(this));
+    window.removeEventListener('resize', this.updateNavbarVisibility.bind(this));
+  }
+
+  scroll() {
+    console.log("Scroll event triggered");
+    const shouldBeScrolled = window.scrollY > this.initialOffsetTop;
+    if (shouldBeScrolled !== this.scrolled) {
+      this.scrolled = shouldBeScrolled;
+      if (this.scrolled) {
+        this.navbarTarget.classList.add('scrolled');
+      } else {
+        this.navbarTarget.classList.remove('scrolled');
+      }
+    }
+  }
+
+  updateNavbarVisibility() {
+    console.log("Updating navbar visibility");
+    if (window.innerWidth <= 420) {
+      this.iconTarget.classList.remove('hidden');
+      this.detailsTarget.classList.add('hidden');
+    } else {
+      this.iconTarget.classList.add('hidden');
+      this.detailsTarget.classList.remove('hidden');
+    }
+  }
+}
